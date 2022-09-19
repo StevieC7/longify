d2aa88bbbdae73da58b204d826f2b0d3217adf80
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-19 11:06:23 -0500

    Handle receiving the token, parsing, storing in local, and redirecting

--- src/pages/Callback.jsx
+++ src/pages/Callback.jsx
@@ -1,3 +1,36 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 export default function Callback(props) {
-    return(<><p>Callback</p></>)
+    const navigate = useNavigate()
+    // write a regex to get the key-value pairs out of the url
+    const getParametersFromHash = (url, parameter) => {
+        let myRegex = new RegExp(parameter + '=([^&]+)(&|$)')
+        let match = url.match(myRegex);
+        return(match ? match[1] : 'none found');
+    }
+    // store the state and access token which I'll need later
+    const state = getParametersFromHash(useLocation().hash, 'state')
+    const accessToken = getParametersFromHash(useLocation().hash, 'access\_token')
+    localStorage.setItem('accessTokenKey', accessToken)
+
+    
+    // TODO: write something to kick people out if the state returned doesn't match the state stored in local browser storage
+    useEffect(() => {
+        navigate('/make')
+    })
+
+    if (state !== localStorage.getItem('spotifyStateKey')) {
+        return(
+            <>
+            <p>Something has gone wrong. No hacking, please.</p>
+        </>
+        )
+    }
+    // redirect the user here using navigate 
+    return(
+        <>
+            <p>This is where I'd put a redirect. IF I HAD ONE</p>
+            <p>{accessToken}</p>
+        </>
+    )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,23 @@
+import { useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 export default function Home(props) {
-    return(<><p>Home</p></>)
+    // Auth API call function that builds query and pings Spotify auth API
+    // refactor later with backend to keep this a real secret, since React will just embed env variables at build
+    const clientID = '83142baf3b354b7a901a4f46e0f371d1'
+    const redirectURL = encodeURIComponent('http://localhost:3000/callback')
+    let scope = encodeURIComponent('user-read-private user-read-email playlist-read-private playlist-modify-private user-library-read user-top-read user-read-recently-played')
+    // set state in local storage at home page render
+    let spotifyState = encodeURIComponent(uuidv4());
+    useEffect(() => {
+        console.log('use effect ran')
+        localStorage.setItem("spotifyStateKey", spotifyState);
+    }, [spotifyState])
+    
+    // click handler for the "start" button that triggers the auth API call
+    return(
+    <>
+        <p>Home</p>
+        <button><a href={`https://accounts.spotify.com/authorize?response_type=token&client_id=${clientID}&scope=${scope}&redirect_uri=${redirectURL}&state=${spotifyState}`}>Build Playlist</a></button>
+    </>
+    )
 }
