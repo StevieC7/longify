f3319f8c6e9d37be58e6ce91fb86d2f6e7bb0635
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-21 15:07:16 -0500

    Style the generated playlist

--- src/components/PlaylistItems.jsx
+++ src/components/PlaylistItems.jsx
@@ -3,13 +3,22 @@ export default function PlaylistItems({playlist}) {
         <div className="playlist">
             {playlist.map((val, key) => {return(
                 val.name ? 
-                    <div key={key} className='playlist-song'>
-                        <p>{val.name}</p>
-                        <p className="song-tag">Song</p>
-                        <p>{`${Math.floor(val.duration_ms / 1000 / 60)}:${(val.duration_ms / 1000 / 60) % (val.duration_ms / 1000 / 60)}`}</p>
+                    <div key={key} className='playlist-item'>
+                        <div className="playlist-item-left">
+                            <p className="song-tag">Song</p>
+                            <p>{val.name}</p>
+                        </div>
+                            <p>{`${Math.floor(val.duration_ms / 1000 / 60)}:${Math.round(((val.duration_ms / 1000 / 60) % (Math.floor(val.duration_ms / 1000 / 60))) * 60)}`}</p>
                     </div>
                 : 
-                    <p key={key}>{val.episode.name}</p>
+                    <div key={key} className='playlist-item'>
+                        <div className="playlist-item-left">
+                            <p className="episode-tag">Podcast</p>
+                            <img className="episode-img" src={val.episode.images[0].url} alt={val.episode.name}></img>
+                            <p>{val.episode.name}</p>
+                        </div>
+                            <p>{`${Math.floor(val.episode.duration_ms / 1000 / 60)}:${Math.round(((val.episode.duration_ms / 1000 / 60) % (Math.floor(val.episode.duration_ms / 1000 / 60))) * 60)}`}</p>
+                    </div>
             )})}
         </div>
     )
