5a5244b60c6dc5126aac0c5e279e12494f11d324
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-21 10:17:32 -0500

    Adjusted some more

--- netlify/functions/keys.js
+++ netlify/functions/keys.js
@@ -1,9 +1,9 @@
 exports.handler = async function(event, context) {
     return {
-        headers: {
+        'statusCode': 200,
+        'headers': {
             'Content-Type': 'application/json'
         },
-        statusCode: 200,
-        body: JSON.stringify({clientID: process.env.clientID})
+        'body': JSON.stringify({'clientID': process.env.clientID})
     }
 }
