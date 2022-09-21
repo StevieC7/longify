df9a8323103e2b9ee6185b817fe6bf7e8d991061
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-21 11:52:25 -0500

    Fix keys function to properly stringify values

--- netlify/functions/keys.js
+++ netlify/functions/keys.js
@@ -4,6 +4,7 @@ exports.handler = async function(event, context) {
         'headers': {
             'Content-Type': 'application/json'
         },
-        'body': JSON.stringify({'clientID': process.env.clientID})
+        // 'body': JSON.stringify({clientID: process.env.clientID})
+        body: JSON.stringify({'clientID': `${process.env.clientID}`})
     }
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,9 @@ import { Button } from '@mui/material';
 export default function Home(props) {
     // Auth API call function that builds query and pings Spotify auth API
     // refactor later with backend to keep this a real secret, since React will just embed env variables at build
-    const getToken = async () => {
-        let token;
-        const response = await fetch('/.netflify/functions/keys')
-        // const clientID = '83142baf3b354b7a901a4f46e0f371d1'
-        console.log('response:',response)
-        token = await response
-        console.log('token:',token)
-        return token
-    }
-    const clientID = getToken()
+    let clientID;
+    fetch('/.netlify/functions/keys').then((res) => res.json()).then((data) => console.log(data))
+
     const redirectURL = encodeURIComponent('https://splendid-dieffenbachia-118c6c.netlify.app/callback')
     // const redirectURL = encodeURIComponent('http://localhost:3000/callback')
     let scope = encodeURIComponent('user-read-private user-read-email playlist-read-private playlist-modify-public user-library-read user-top-read user-read-recently-played')
