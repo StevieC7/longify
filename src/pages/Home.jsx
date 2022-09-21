86c12525806b55ab57d6cda7acf34cc2e28530e5
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-21 09:07:43 -0500

    Enclose netlify function call in async await

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,12 @@ import { Button } from '@mui/material';
 export default function Home(props) {
     // Auth API call function that builds query and pings Spotify auth API
     // refactor later with backend to keep this a real secret, since React will just embed env variables at build
-    let clientID;
-    fetch('/.netflify/functions/keys').then((res) => clientID = res)
-    // const clientID = '83142baf3b354b7a901a4f46e0f371d1'
+    const getToken= async () => {
+        const clientID = await fetch('/.netflify/functions/keys').then((res) => res.json().clientID)
+        // const clientID = '83142baf3b354b7a901a4f46e0f371d1'
+        return clientID
+    }
+    getToken()
     const redirectURL = encodeURIComponent('https://splendid-dieffenbachia-118c6c.netlify.app/callback')
     // const redirectURL = encodeURIComponent('http://localhost:3000/callback')
     let scope = encodeURIComponent('user-read-private user-read-email playlist-read-private playlist-modify-public user-library-read user-top-read user-read-recently-played')
