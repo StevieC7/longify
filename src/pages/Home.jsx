e99297002eea5fc8e74ffd7e49dc7e95f035c709
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-21 09:14:29 -0500

    Store result of function call in variable so it is accessible

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,17 @@ export default function Home(props) {
     // Auth API call function that builds query and pings Spotify auth API
     // refactor later with backend to keep this a real secret, since React will just embed env variables at build
     const getToken= async () => {
-        const clientID = await fetch('/.netflify/functions/keys').then((res) => res.json().clientID)
+        const token = await fetch('/.netflify/functions/keys').then((res) => res.json().clientID)
         // const clientID = '83142baf3b354b7a901a4f46e0f371d1'
-        return clientID
+        return token
     }
+    const clientID = getToken()
     const redirectURL = encodeURIComponent('https://splendid-dieffenbachia-118c6c.netlify.app/callback')
     // const redirectURL = encodeURIComponent('http://localhost:3000/callback')
     let scope = encodeURIComponent('user-read-private user-read-email playlist-read-private playlist-modify-public user-library-read user-top-read user-read-recently-played')
     // set state in local storage at home page render
     let spotifyState = encodeURIComponent(uuidv4());
     useEffect(() => {
-        getToken()
         localStorage.setItem("spotifyState", spotifyState);
     }, [spotifyState])
     
