cd98226463fec857d4bf1d7a411445da98f443ac
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-21 13:54:28 -0500

    remove console logs from env API debacle

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
 export default function StartButton({auth, setAuth}) {
     const redirectURL = encodeURIComponent('https://splendid-dieffenbachia-118c6c.netlify.app/callback')
-    // const redirectURL = encodeURIComponent('http://localhost:3000/callback')
     const scope = encodeURIComponent('user-read-private user-read-email playlist-read-private playlist-modify-public user-library-read user-top-read user-read-recently-played')
     // set state in local storage at home page render
     const spotifyState = encodeURIComponent(uuidv4());
@@ -13,7 +12,6 @@ export default function StartButton({auth, setAuth}) {
         fetch('/.netlify/functions/keys')
         .then((res) => res.json())
         .then((data) => {
-            console.log(data.clientID)
             setAuth({clientID: data.clientID, redirectURL: redirectURL, scope: scope, spotifyState: spotifyState})
         })
         // eslint-disable-next-line
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,32 +2,7 @@ import { useState } from 'react';
 import StartButton from '../components/StartButton';
 export default function Home(props) {
     const [auth, setAuth] = useState({})
-    // Auth API call function that builds query and pings Spotify auth API
-    // refactor later with backend to keep this a real secret, since React will just embed env variables at build
-    // fetch('/.netlify/functions/keys')
-    // .then((res) => res.json())
-    // .then((data) => {
-    //     console.log(data)
-    //     clientID = data.clientID
-    //     console.log(clientID)
-    // })
 
-    // const redirectURL = encodeURIComponent('https://splendid-dieffenbachia-118c6c.netlify.app/callback')
-    // // const redirectURL = encodeURIComponent('http://localhost:3000/callback')
-    // let scope = encodeURIComponent('user-read-private user-read-email playlist-read-private playlist-modify-public user-library-read user-top-read user-read-recently-played')
-    // // set state in local storage at home page render
-    // let spotifyState = encodeURIComponent(uuidv4());
-    // useEffect(() => {
-    //     // fetch('/.netlify/functions/keys')
-    //     // .then((res) => res.json())
-    //     // .then((data) => {
-    //     //     console.log(data.clientID)
-    //     //     setAuth({clientID: data.clientID, redirectURL: redirectURL, scope: scope})
-    //     // })
-    //     localStorage.setItem("spotifyState", spotifyState);
-    // }, [spotifyState])
-    
-    // click handler for the "start" button that triggers the auth API call
     return(
     <>
         <h1>Longify</h1>
