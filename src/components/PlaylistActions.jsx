45694f3ccc9cff1b9f30300faa2e834d07c48b0f
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-22 12:11:03 -0500

    Create split function to split playlist items into multiple objects for use in request

--- src/components/PlaylistActions.jsx
+++ src/components/PlaylistActions.jsx
@@ -11,6 +11,25 @@ export default function PlaylistActions({ playlist, setPlaylist, userConfig }) {
             }
             return `spotify:episode:${val.episode.id}`
         })}
+    // add function to split the list up until all URI lists would be shorter than 100 items (limit set by Spotify API)
+    if (spotifyURIs.uris.length > 0) {
+        let arrSpotifyURIs = [spotifyURIs, spotifyURIs];
+        console.log(arrSpotifyURIs)
+        let i = 0;
+        while (arrSpotifyURIs[i].uris.length > 100 && arrSpotifyURIs[i].uris.length) {
+            // below creates new object containing the excess
+            arrSpotifyURIs.push({uris: arrSpotifyURIs[i].uris.slice(100)})
+            // iterate to the new array and do it again
+            i++
+        }
+        // below splices only the first 100 in place in any spot where there are more than 100 (these were previously sliced and had the excess pushed to the end of the array)
+        let j = 0;
+        while (arrSpotifyURIs[j].uris.length > 100 && arrSpotifyURIs[j].uris.length) {
+            arrSpotifyURIs[j] = {uris: arrSpotifyURIs[j].uris.slice(0,100)}
+            j++
+        }
+        console.log(arrSpotifyURIs)
+    }
     const getSongMixInfo = (num) => {
         if (num === 50) {
             return 'even mix'
