8256f4b7b3483cb5bc91133453922168e070915f
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-21 22:15:45 -0500

    testing new time format

--- src/components/PlaylistItems.jsx
+++ src/components/PlaylistItems.jsx
@@ -1,4 +1,17 @@
 export default function PlaylistItems({playlist}) {
+    function formatDuration (ms) {
+        let totalSeconds = ms / 1000;
+        console.log('initial totalSeconds:',totalSeconds)
+        let totalMinutes = totalSeconds / 60;
+        console.log('initial totalMinutes:',totalMinutes)
+        let totalHours = totalMinutes / 60;
+        console.log('initial totalHours:', totalHours)
+        totalMinutes = (totalMinutes % 60) * 60
+        totalHours = Math.floor(totalHours)
+        totalSeconds = totalSeconds - (totalHours * 60 * 60) - (totalMinutes * 60)
+        return `${totalHours >= 10 ? totalHours : `0${totalHours}`}:${totalMinutes >= 10 ? totalMinutes : `0${totalMinutes}`}:${totalSeconds >= 10 ? totalSeconds : `0${totalSeconds}`}`
+
+    }
     return(
         <div className="playlist">
             {playlist.map((val, key) => {return(
@@ -8,7 +21,8 @@ export default function PlaylistItems({playlist}) {
                             <p className="song-tag">Song</p>
                             <p>{val.name}</p>
                         </div>
-                            <p>{`${Math.floor(val.duration_ms / 1000 / 60)}:${Math.round(((val.duration_ms / 1000 / 60) % (Math.floor(val.duration_ms / 1000 / 60))) * 60)}`}</p>
+                            {/* <p>{`${Math.floor(val.duration_ms / 1000 / 60)}:${Math.round(((val.duration_ms / 1000 / 60) % (Math.floor(val.duration_ms / 1000 / 60))) * 60)}`}</p> */}
+                            <p>{formatDuration(val.duration_ms)}</p>
                     </div>
                 : 
                     <div key={key} className='playlist-item'>
