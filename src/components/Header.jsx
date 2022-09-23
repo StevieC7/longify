541945a9dd1605f7e70b16365f7903f12903e026
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-22 21:38:09 -0500

    Add logo and some styling for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ export default function Header(props) {
     return(
         <header>
             <div className="header-inner">
+                <img src={`${process.env.PUBLIC_URL}/longify512.png`} className="logo" alt="logo"></img>
                 <h1>Longify</h1>
                 <p>A long playlist maker for Spotify</p>
             </div>
