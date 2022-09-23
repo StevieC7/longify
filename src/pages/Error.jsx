e5c523a28978ca34502e43f8938027146a7fbe98
StevieC7 <stevenrcalhoun@outlook.com>
2022-09-22 22:27:07 -0500

    Improve error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,6 @@
+import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import Header from '../components/Header'
 
 export default function Error(props) {
     const navigate = useNavigate()
@@ -8,8 +10,12 @@ export default function Error(props) {
     }
     return(
         <div className="error">
-            <p>An error has occurred. Please return to homepage, log in to Spotify, and try again.</p>
-            <button onClick={() => handleClick()}>Try Again</button>
+            <div className="page-wrapper">
+                <Header userConfig={{songMix: undefined}}></Header>
+                <h2>Error: reauthentication required</h2>
+                <p>Spotify requires you to sign in again for security reasons.</p>
+                <Button variant='contained' sx={{margin: '2rem', backgroundColor: '#1DB954', '&:hover': {backgroundColor: '#1DB954'}}} onClick={() => handleClick()}>Sign in</Button>
+            </div>
         </div>
     )
 }
