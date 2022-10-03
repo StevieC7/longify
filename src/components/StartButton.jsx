18719fac2543c57fcaf9f84d092d7f2d19e0736d
StevieC7 <stevenrcalhoun@outlook.com>
2022-10-02 20:09:37 -0500

    Achieve duct tape solution

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,15 +1,25 @@
 import { Button } from "@mui/material"
+import { useEffect, useState } from "react"
+
+
 export default function StartButton({auth, setAuth}) {
-    
-    const getRedirectURL = async () => {
-        return await fetch('/.netlify/functions/auth').then((res) => res.json().then((json) => json))
-    }
-    console.log(getRedirectURL())
+    const [redir, setRedir] = useState(null)
+    useEffect(() => {
+        const getRedirectURL = async () => {
+            const response = await fetch('/.netlify/functions/auth')
+            const data = await response.json()
+            return data
+        }
+        // const resolveURL = async () => {
+            //     const redirectURL = await getRedirectURL().then(data => data)
+            // }
+            getRedirectURL().then(data => setRedir(data))
+    }, [])
     return(
         <Button 
             variant="contained" 
             sx={{margin: '2rem', backgroundColor: '#1DB954', '&:hover': {backgroundColor: '#1DB954'}}}
-            href='#'
+            href={redir !== null ? redir.redirect : '#'}
         >
             Log in to Spotify
         </Button>
